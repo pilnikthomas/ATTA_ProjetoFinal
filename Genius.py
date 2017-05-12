3fc502116d27b46bb53d2e5641a7b091f55e763a
TiagoMingossi <tiagorm@al.insper.edu.br>
2017-05-12 08:01:07 -0300

    colocando imagem inicial

--- Genius.py
+++ Genius.py
@@ -6,22 +6,30 @@ Created on Sat May  6 17:23:45 2017
 """
 
 import pygame
+import tkinter as tk
 from pygame.locals import *
 import sys
 
-pygame.init()
-screen = pygame.display.set_mode((1500, 1500))
-image = pygame.image.load('simon.jpg')
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            running = False
-            
-    screen.fill((200, 200, 200))
-    screen.blit(image, (750, 750))
-    pygame.display.flip()
-        
+		
+		
+
+def tela_inicial():
+	pygame.init()
+	tela = pygame.display.set_mode((234,234))
+	pygame.display.set_caption("Jogo Genius")
+	fundo = pygame.image.load("imagens/genius.png").convert()
+	#botao = pygame.Button(tela)
+	#botao.configure(text="Iniciar")
+	#botao.grid()
+	
+	while True:
+		for evento in pygame.event.get():
+			if evento.type == QUIT:
+				pygame.quit()
+				sys.exit()
+			tela.blit(fundo,(0,0))
+			#botao.blit(fundo,(234/2,234/2))
+		pygame.display.update()
+		
+print(tela_inicial())
