948b06c94e66adafd6b610c7e18b81acbaa17097
TiagoMingossi <tiagorm@al.insper.edu.br>
2017-05-26 09:12:11 -0300

    jogo teste

--- Geniusteste.py
+++ Geniusteste.py
@@ -3,6 +3,9 @@ from pygame.locals import *
 import sys
 import random 
 
+
+	
+	
 def tela_inicial():
 	pygame.init()
 	tela = pygame.display.set_mode((600,584))
@@ -37,76 +40,87 @@ def tela_inicial():
 		
 			
 		print(pos_mouse)
-		
+		botoes_mouse= pygame.mouse.get_pressed()
 		if x>250 and x<350 and y>242 and y<342:
 			print("botao")
-		botoes_mouse1= pygame.mouse.get_pressed()
-		if botoes_mouse1[0]:
-			def tela_jogo():
-				pygame.init()
-				tela = pygame.display.set_mode((600,586))
-				pygame.display.set_caption("Genius Game")
-				botao1 = pygame.image.load("botao_azul.jpg").convert()
-				botao2 = pygame.image.load("botao_vermelho.jpg").convert()
-				botao3 = pygame.image.load("botao_amarelo.jpg").convert()
-				botao4 = pygame.image.load("botao_verde.jpg").convert()
-				botao5 = pygame.image.load("botao_azul_claro.jpg").convert()
-				botao6 = pygame.image.load("botao_vermelho_claro.jpg").convert()
-				botao7 = pygame.image.load("botao_amarelo_claro.jpg").convert()
-				botao8 = pygame.image.load("botao_verde_claro.jpg").convert()
-				while True:
-					for evento in pygame.event.get():
-						if evento.type == QUIT:
-							pygame.quit()
-							sys.exit()
-						tela.blit(botao1,(0,0))
-						tela.blit(botao2,(300,0))
-						tela.blit(botao3,(0,300))
-						tela.blit(botao4,(300,300))
-						
-					pos_mouse= pygame.mouse.get_pos()
-					x= pos_mouse[0]
-					y= pos_mouse[1]
+			if botoes_mouse[0]:
+				def tela_jogo():
+					pygame.init()
+					tela = pygame.display.set_mode((600,586))
+					pygame.display.set_caption("Genius Game")
+					botao1 = pygame.image.load("botao_azul.jpg").convert()
+					botao2 = pygame.image.load("botao_vermelho.jpg").convert()
+					botao3 = pygame.image.load("botao_amarelo.jpg").convert()
+					botao4 = pygame.image.load("botao_verde.jpg").convert()
+					botao5 = pygame.image.load("botao_azul_claro.jpg").convert()
+					botao6 = pygame.image.load("botao_vermelho_claro.jpg").convert()
+					botao7 = pygame.image.load("botao_amarelo_claro.jpg").convert()
+					botao8 = pygame.image.load("botao_verde_claro.jpg").convert()
 					
-					if x>0 and x<300 and y>0 and y<293:
-						print("azul")
-					botoes_mouse1= pygame.mouse.get_pressed()
-					if botoes_mouse1[0]:
-						print("azul")
-						tela.blit(botao5,(0,0))
+					while True:
+						for evento in pygame.event.get():
+							if evento.type == QUIT:
+								pygame.quit()
+								sys.exit()
+							tela.blit(botao1,(0,0))
+							tela.blit(botao2,(300,0))
+							tela.blit(botao3,(0,300))
+							tela.blit(botao4,(300,300))
 						
-					if x>301 and x<600 and y>0 and y<293:
-						print("vermelho")
-					botoes_mouse2= pygame.mouse.get_pressed()
-					if botoes_mouse1[0]:
-						print("vermelho")
-						tela.blit(botao6,(300,0))
+						pos_mouse= pygame.mouse.get_pos()
+						x= pos_mouse[0]
+						y= pos_mouse[1]
 						
-					if x>0 and x<300 and y>294 and y<586:
-						print("amarelo")
-					botoes_mouse3= pygame.mouse.get_pressed()
-					if botoes_mouse1[0]:
-						print("amarelo")
-						tela.blit(botao7,(0,300))
+						botoes_mouse= pygame.mouse.get_pressed()
+						if x>0 and x<300 and y>0 and y<293:
+							#print("azul")
+							if botoes_mouse[0]:
+								tela.blit(botao5,(0,0))
+								print("azul")
+						if x>301 and x<600 and y>0 and y<293:
+							#print("vermelho")
+							if botoes_mouse[0]:
+								tela.blit(botao6,(300,0))
+								print("vermelho")
+						if x>0 and x<300 and y>294 and y<586:
+							#print("amarelo")
+							if botoes_mouse[0]:
+								tela.blit(botao7,(0,300))
+								print("amarelo")
+						if x>301 and x<600 and y>294 and y<586:
+							#print("verde")	
+							if botoes_mouse[0]:
+								tela.blit(botao8,(300,300))
+								print("verde")
+							
+						pygame.display.update()
 						
-					if x>301 and x<600 and y>294 and y<586:
-						print("verde")
-					botoes_mouse4= pygame.mouse.get_pressed()
-					if botoes_mouse1[0]:
-						print("verde")
+				print(tela_jogo())
+				
+				
+				def pisca(nome):
+					if nome=="verde":
 						tela.blit(botao8,(300,300))
 						
-					pygame.display.update()
-					
-			print(tela_jogo())
-
+			#a=input("escolha um botao")
+			#pisca(a)
 		if x>500 and x<600 and y>516 and y<586:
 			print("ranking")
-		botoes_mouse2= pygame.mouse.get_pressed()
-		if botoes_mouse2[0]:
-				print("ranking")
+			if botoes_mouse[0]:
+				def tela_ranking():
+					pygame.init()
+					tela = pygame.display.set_mode((600,586))
+					pygame.display.set_caption("Genius Game")
 					
-		
+					while True:
+						for evento in pygame.event.get():
+							if evento.type == QUIT:
+								pygame.quit()
+								sys.exit()
+								
+						pygame.display.update()
+						
+				print(tela_ranking())
 		
 		pygame.display.update()
 		
