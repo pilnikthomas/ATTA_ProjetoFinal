18d686891e94815a33e098d516ff400cb99880a9
iglicky10 <iglicky10@gmail.com>
2017-06-01 18:47:05 -0300

    tela inicial nao funcional

--- tela inicial genius pygame.py
+++ tela inicial genius pygame.py
@@ -5,22 +5,30 @@ from pygame.locals import *
 
 
 pg.init()
-tela = pg.display.set_mode((1000,800))
+tela = pg.display.set_mode((600,600))
 pg.display.set_caption('Tela inicial Genius')
 tela_rect = tela.get_rect()
 
 #rect single player
-rect = pg.Rect(150,400, 300, 150)
+rect = pg.Rect(25, 350, 250, 100)
 
 pg.draw.rect(tela, pg.Color("blue"), rect, 10)
 pg.display.flip()
 
 
 #rect multiplayer
-rect = pg.Rect(550,400, 300, 150)
+rect = pg.Rect(325,350, 250, 100)
 
 pg.draw.rect(tela, pg.Color("red"), rect, 10)
 pg.display.flip()
+
+#rect credits
+rect = pg.Rect(225,480, 150, 75)
+
+pg.draw.rect(tela, pg.Color("green"), rect, 10)
+pg.display.flip()
+
+'''
 
 #rect configurações
 rect = pg.Rect(150,600, 300, 150)
@@ -35,16 +43,28 @@ rect = pg.Rect(550,600, 300, 150)
 pg.draw.rect(tela, pg.Color("green"), rect, 10)
 pg.display.flip()
 
+'''
+
 #texto singleplayer
-fonte = pg.font.SysFont("dejavusans" , 50)
+fonte = pg.font.SysFont("dejavusans" , 30)
 texto1 = fonte.render('Single Player', True, (0,0,255))
-tela.blit(texto1, (190, 420))
+tela.blit(texto1, (55, 380))
 pg.display.update()
 
 #texto multiplayer
+fonte = pg.font.SysFont("dejavusans" , 30)
 texto2 = fonte.render('Multiplayer', True, (255,0,0))
-tela.blit(texto2, (610, 420))
+tela.blit(texto2, (365, 380))
 pg.display.update()
+
+#texto credits
+fonte = pg.font.SysFont("dejavusans" , 30)
+texto2 = fonte.render('Credits', True, (0,255,0))
+tela.blit(texto2, (250, 500))
+pg.display.update()
+
+
+'''
 
 #texto configurações
 texto3 = fonte.render('Configurações', True, (255,255,0))
@@ -60,11 +80,13 @@ pg.display.update()
 texto4 = fonte.render('Ranking', True, (0,255,0))
 tela.blit(texto4, (630, 620))
 pg.display.update()
-        
+      
+'''
+	  
 #texto título
-fonte2 = pg.font.SysFont("kristenitc" , 88)
+fonte2 = pg.font.SysFont("kristenitc" , 75)
 titulo = fonte2.render('Genius Game', True, (255,200,0))
-tela.blit(titulo, (210, 110))
+tela.blit(titulo, (55, 110))
 pg.display.update()
 
 
